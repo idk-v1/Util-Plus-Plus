de5d7f5c033edfed652158838f1a29988b14eae5
idk-v1 <benhamilton.jpg@gmail.com>
2024-02-27 15:03:12 -0600

    Now has permissions to shut down

--- Programs/RstWOUpd/RstWOUpd.cpp
+++ Programs/RstWOUpd/RstWOUpd.cpp
@@ -6,7 +6,20 @@ int main()
 		MB_OKCANCEL | MB_ICONQUESTION | MB_DEFBUTTON2) == IDOK)
 	{
 		MessageBoxW(NULL, L"restarting", 0, 0);
-		InitiateShutdownA(NULL, (LPSTR)"Restarting", 5, SHUTDOWN_RESTART, SHTDN_REASON_FLAG_PLANNED);
+
+        HANDLE hToken = NULL;
+        LUID luid;
+        TOKEN_PRIVILEGES tp;
+
+        OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken);
+        LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &luid);
+
+        tp.PrivilegeCount = 1;
+        tp.Privileges[0].Luid = luid;
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+        AdjustTokenPrivileges(hToken, false, &tp, sizeof(tp), NULL, 0);
+
+        InitiateSystemShutdownA(NULL, NULL, 0, true, true);
 	}
 	return 0;
 }
