69af848749e609090c11fed28966ecd7eedde151
idk-v1 <benhamilton.jpg@gmail.com>
2024-03-08 09:31:20 -0600

    Version numbering in titlebar

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,10 @@
 #include "Utils/UIx1.h"
 #include "Utils/File.h"
 
+#define VERSION_MAJOR 1 // Compatibility
+#define VERSION_MINOR 6 // Feature
+#define VERSION_PATCH 1 // Bug Fix
+//#define DEBUG 1			// Work in Progress
 
 int main()
 {
@@ -8,7 +12,18 @@ int main()
 	sf::Vector2u winSize;
 	UIx1::UI ui("res/.ui", &winSize);
 
-	sf::RenderWindow win(sf::VideoMode(winSize.x, winSize.y), "Maybe helpful things... IDK", sf::Style::Close);
+	std::string version = "v";
+	version += VERSION_MAJOR + '0';
+	version += '.';
+	version += VERSION_MINOR + '0';
+	version += '.';
+	version += VERSION_PATCH + '0';
+#ifdef DEBUG
+		version += " - Debug";
+#endif
+
+
+	sf::RenderWindow win(sf::VideoMode(winSize.x, winSize.y), "Util-Plus-Plus - " + version, sf::Style::Close);
 	win.setFramerateLimit(20);
 	while (win.isOpen())
 	{
