7f5e40ed0b50690f5f0f4bdb4375b9970b138d3c
idk-v1 <benhamilton.jpg@gmail.com>
2024-03-07 22:24:27 -0600

    Latest release now updates without user needing to know how to extract zip files

--- Misc/LastRel/LastRel.cpp
+++ Misc/LastRel/LastRel.cpp
@@ -1,8 +1,44 @@
 #include <windows.h>
-#include <shellapi.h>
+#include <urlmon.h>
+#pragma comment(lib,"urlmon.lib")
+
+#include "../../Utils/File.h"
+#include "../../Utils/Process.h"
+#include "../../Utils/miniz.c"
 
 int main()
 {
-	ShellExecuteA(0, 0, "https://github.com/idk-v1/Util-Plus-Plus/releases/latest", 0, 0, SW_SHOW);
+    setCurrentDir(getProgDir() + "\\..\\");
+
+    if (!URLDownloadToFileA(0,
+        "https://github.com/idk-v1/Util-Plus-Plus/releases/latest/download/util-plus-plus.zip",
+        "temp.zip",
+        0, NULL))
+    {
+        killParentProc();
+
+        std::string file;
+        mz_zip_archive zip;
+        memset(&zip, 0, sizeof(zip));
+        mz_zip_reader_init_file(&zip, "temp.zip", 0);
+        
+        for (int i = 0; i < mz_zip_reader_get_num_files(&zip); i++)
+        {
+            mz_zip_archive_file_stat file_stat;
+            if (!mz_zip_reader_file_stat(&zip, i, &file_stat))
+                break;
+
+            file = file_stat.m_filename;
+            if (file.find('\\') != std::string::npos)
+                mkDir(getCurrentDir() + "\\" + file.substr(0, file.find('\\')));
+            mz_zip_reader_extract_file_to_file(&zip, file_stat.m_filename,
+                std::string(getCurrentDir() + "\\" + file_stat.m_filename).data(), 0);
+        }
+        mz_zip_reader_end(&zip);
+        remove("temp.zip");
+
+        startProc("Util-Plus-Plus.exe");
+    }
+
 	return 0;
 }
